707a9a83ad3a22290877c7db7685f741422b5d77
Animesh Singh <animeshsingrol@gmail.com>
2023-12-23 13:20:02 +0530

    done

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+const btn1 = document.getElementById("resume-link-1")
+function myFunction() {
+  window.open(
+    "https://drive.google.com/file/d/1g5JAMhaQuJQGt3V5Qi7hVmdtOSzsKpby/view?usp=drive_link"
+  )
+}
+
+const btn2 = document.getElementById("resume-link-2")
+function myFunction2() {
+  window.open(
+    "https://drive.google.com/file/d/1g5JAMhaQuJQGt3V5Qi7hVmdtOSzsKpby/view?usp=drive_link"
+  )
+}
